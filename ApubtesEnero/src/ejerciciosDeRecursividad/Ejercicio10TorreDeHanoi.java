59d494d14c2d36cdec78f4db4d536126457b22e1
ThayroneSal <thayrone.saldanha@gmail.com>
2025-01-16 19:43:17 +0100

    Ejercicio10
    Se ha corregido un apartado del ejercicio 10 torre de Hanói.

--- ApubtesEnero/src/ejerciciosDeRecursividad/Ejercicio10TorreDeHanoi.java
+++ ApubtesEnero/src/ejerciciosDeRecursividad/Ejercicio10TorreDeHanoi.java
@@ -13,11 +13,12 @@ Implementa la solución al problema clásico de la Torre de Hanói para ( n ) di
 public static void resolverTorre (int n, char origen, char auxiliar, char destino){
 
 if (n==1){
-    System.out.println("Mover disco 1 de " +origen+ "a" + destino);
+    System.out.println("Mover disco 1 de " +origen+ " a "  + destino);
+    return;
 }
 resolverTorre (n-1, origen, destino, auxiliar); //resulve la torre inmediatamente menos a esta
 
-System.out.println("Mover disco " +n+ "de " +origen+ " a " +destino);
+System.out.println("Mover disco " +n+ " de " +origen+ " a " +destino);
 
 resolverTorre (n-1, auxiliar, origen, destino );
   }
