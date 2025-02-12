32b519b3ad856139cc9e5e83deafaaaa5ed835af
ThayroneSal <thayrone.saldanha@gmail.com>
2025-02-12 20:38:46 +0100

    proyecto Maven
    revisar

---
+++ proyecto_maven/src/main/java/com/example/com/example/App.java
@@ -0,0 +1,25 @@
+/*package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+/*public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}*/
+ package com.example;
+
+import org.apache.commons.lang3.StringUtils; // Importamos la clase de Apache Commons Lang
+
+public class App {
+    public static void main(String[] args) {
+        String mensaje = "Hello World!";
+        // Convierte el mensaje a mayúsculas usando StringUtils
+        String mensajeEnMayusculas = StringUtils.upperCase(mensaje);
+        System.out.println(mensajeEnMayusculas);
+    }
+}
---
+++ proyecto_maven/src/main/java/com/example/com/example/StringUtils.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class StringUtils {
+
+    public static String upperCase(String mensaje) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'upperCase'");
+    }
+
+}
